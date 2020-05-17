16bcfa52e76218fac954ad33da048404116dda6a
DoLim <limdongyoung0@gmail.com>
2020-05-17 09:51:48 +0000

    fix: typo fix

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -4,7 +4,6 @@ export const getToken = async () => {
   const keyData = {
     key: process.env.REACT_APP_KEY,
   };
-  console.log(keyData);
   const {
     data: { token },
   } = await axios.post(`${process.env.REACT_APP_URI}/token`, keyData, {
@@ -17,36 +16,6 @@ export const getToken = async () => {
 };
 
 export const getData = async ({ token, value }: any) => {
-  // const example = {
-  //   drivers: [
-  //     {
-  //       avail_radius: 0,
-  //       id: "string",
-  //       position: {
-  //         id: "string",
-  //         lat: 0,
-  //         long: 0,
-  //       },
-  //     },
-  //   ],
-  //   stuffs: [
-  //     {
-  //       id: "string",
-  //       recver_name: "string",
-  //       recver_position: {
-  //         id: "string",
-  //         lat: 0,
-  //         long: 0,
-  //       },
-  //       sender_name: "string",
-  //       sender_position: {
-  //         id: "string",
-  //         lat: 0,
-  //         long: 0,
-  //       },
-  //     },
-  //   ],
-  // };
   const {
     data: { actions },
   } = await axios.post(`${process.env.REACT_APP_URI}/path`, value, {
