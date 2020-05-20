686e77c85e2389eb1032c67b6b6bf06561d73e48
DoLim <limdongyoung0@gmail.com>
2020-05-20 14:09:23 +0000

    feat: apply react-router-dom and show kakaomap

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
 import React from "react";
 import "./App.css";
 import dotenv from "dotenv";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
 import HomePage from "./pages/HomePage";
+import KakaoMap from "./components/KakaoMap";
 
 dotenv.config();
 
 function App() {
-  return <HomePage />;
+  // return <HomePage />;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact={true} path={"/"} component={HomePage}></Route>
+        <Route path={"/map"} component={KakaoMap}></Route>
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/components/KakaoMap/KakaoMap.tsx
@@ -0,0 +1,29 @@
+/*global kakao*/
+import React, { useEffect } from "react";
+import { Box } from "@chakra-ui/core";
+
+declare global {
+  interface Window {
+    kakao: any;
+  }
+}
+
+const KakaoMap = () => {
+  useEffect(() => {
+    let container = document.getElementById("map");
+    let options = {
+      center: new window.kakao.maps.LatLng(33.450701, 126.570667),
+      level: 3,
+    };
+
+    let map = new window.kakao.maps.Map(container, options);
+  }, []);
+
+  return (
+    <>
+      <Box id="map" width="500px" height="400px"></Box>
+    </>
+  );
+};
+
+export default KakaoMap;
