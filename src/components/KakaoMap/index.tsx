f60b4b0f790b8c2a97e2c2c28f59448076421750
DoLim <limdongyoung0@gmail.com>
2020-05-20 14:23:02 +0000

    feat: applied kakao map sample about multi-marker and map-control

--- src/components/KakaoMap/index.tsx
+++ src/components/KakaoMap/index.tsx
@@ -1,3 +1,16 @@
+import React from "react";
 import MapBox from "./MapBox";
+import Layout from "../Layout";
+import { Box } from "@chakra-ui/core";
 
-export default MapBox;
+const KakaoMap = () => {
+  return (
+    <Layout>
+      <Box width="100%" height="100%">
+        <MapBox width="100%" height="100vh" />
+      </Box>
+    </Layout>
+  );
+};
+
+export default KakaoMap;
