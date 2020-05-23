5525005cc29a4dd2bdeebd96a6fbd6d0c512616f
DoLim <limdongyoung0@gmail.com>
2020-05-23 14:24:20 +0000

    feat: Made map to mark directions using polyline

--- src/components/NaverMap/MapDiv.tsx
+++ src/components/NaverMap/MapDiv.tsx
@@ -1,11 +1,25 @@
 import React from "react";
-import { NaverMap, Marker } from "react-naver-maps";
+import { NaverMap, Polyline } from "react-naver-maps";
 
 declare global {
   interface Window {
     naver: any;
   }
 }
+
+export type LatLongPair = {
+  lat: number;
+  long: number;
+};
+
+export type LatLongPairArray = LatLongPair[];
+
+// array shape is like [{lat: 123.456, long: 123,4565}, {lat: 123.152, long: 195.125}, ...]
+const arrayToPath = (arr: LatLongPairArray, navermaps: any) => {
+  return arr.map(
+    (pair: LatLongPair) => new navermaps.LatLng(pair.lat, pair.long)
+  );
+};
 
 type MapDivProps = {
   children?: React.ReactNode;
@@ -14,6 +28,7 @@ type MapDivProps = {
   defaultCenterLat?: number;
   defaultCenterLong?: number;
   defaultZoom?: number;
+  path?: LatLongPairArray;
 };
 
 const MapDiv = ({
@@ -23,6 +38,7 @@ const MapDiv = ({
   defaultCenterLat,
   defaultCenterLong,
   defaultZoom,
+  path,
 }: MapDivProps) => {
   const navermaps = window.naver.maps;
 
@@ -36,13 +52,21 @@ const MapDiv = ({
       defaultCenter={{ lat: defaultCenterLat, lng: defaultCenterLong }} // 지도 초기 위치
       defaultZoom={defaultZoom} // 지도 초기 확대 배율
     >
-      <Marker
+      {/* <Marker
         key={1}
         position={new navermaps.LatLng(37.551229, 126.988205)}
         animation={2}
         onClick={() => {
           alert("여기는 N서울타워입니다.");
         }}
+      /> */}
+      <Polyline
+        path={path && arrayToPath(path, navermaps)}
+        // clickable // 사용자 인터랙션을 받기 위해 clickable을 true로 설정합니다.
+        strokeColor={"#5347AA"}
+        strokeStyle={"solid"}
+        strokeOpacity={1}
+        strokeWeight={5}
       />
       {children}
     </NaverMap>
@@ -52,9 +76,17 @@ const MapDiv = ({
 MapDiv.defaultProps = {
   width: "100%",
   height: "100vh",
-  defaultCenterLat: 37.554722,
-  defaultCenterLong: 126.970833,
-  defaultZoom: 13,
+  defaultCenterLat: 37.3646656,
+  defaultCenterLong: 127.108828,
+  defaultZoom: 15,
+  path: [
+    { lat: 37.365620929135716, long: 127.1036195755005 },
+    { lat: 37.365620929135716, long: 127.11353302001953 },
+    { lat: 37.3606921307849, long: 127.10452079772949 },
+    { lat: 37.36821310838941, long: 127.10814714431763 },
+    { lat: 37.360760351656545, long: 127.11299657821654 },
+    { lat: 37.365620929135716, long: 127.1036195755005 },
+  ],
 };
 
 export default MapDiv;
--- src/components/NaverMap/index.tsx
+++ src/components/NaverMap/index.tsx
@@ -2,12 +2,18 @@ import React from "react";
 import Layout from "../Layout";
 import { Box } from "@chakra-ui/core";
 import { RenderAfterNavermapsLoaded } from "react-naver-maps";
-import MapDiv from "./MapDiv";
+import MapDiv, { LatLongPairArray } from "./MapDiv";
 
-const Map = () => {
+type MapProps = {
+  path?: LatLongPairArray;
+  width: string;
+  height: string;
+};
+
+const Map = ({ path, width, height }: MapProps) => {
   return (
     <Layout>
-      <Box width="100%" height="100vh">
+      <Box width={width} height={height}>
         <RenderAfterNavermapsLoaded
           ncpClientId={process.env.REACT_APP_NAVER_MAP_KEY} // 자신의 네이버 계정에서 발급받은 Client ID
           error={<p>Maps Load Error</p>}
@@ -15,9 +21,10 @@ const Map = () => {
         >
           <MapDiv
             width="100%"
-            height="100vh"
-            defaultCenterLat={37.554722}
-            defaultCenterLong={126.970833}
+            height="100%"
+            defaultCenterLat={37.3646656}
+            defaultCenterLong={127.108828}
+            path={path}
           />
         </RenderAfterNavermapsLoaded>
       </Box>
@@ -26,3 +33,8 @@ const Map = () => {
 };
 
 export default Map;
+
+Map.defaultProps = {
+  width: "100%",
+  height: "100vh",
+};
